53cc6a97cd139c7d9d2cbb6fccca53a31994460d
Jake <jne.lacrosse@gmail.com>
2016-07-15 21:54:56 -0400

    updated controller

--- EasyPaymentsWeb/src/main/java/com/easyPayments/controllers/PublicEasyPaymentCalculator.java
+++ EasyPaymentsWeb/src/main/java/com/easyPayments/controllers/PublicEasyPaymentCalculator.java
@@ -1,25 +1,26 @@
 package com.easyPayments.controllers;
 
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.easyPayments.TransferObjects.Payment;
 import com.easyPayments.TransferObjects.PaymentCalculatorInput;
 import com.easyPayments.TransferObjects.PaymentCalculatorOutput;
+import com.easyPayments.services.EasyCalculateService;
 
 @RestController
 public class PublicEasyPaymentCalculator {
 
-	
-	PaymentCalculatorInput input;
-	public void prepareUserExpenses(){
+	@Autowired
+	private EasyCalculateService  calcService;
+	@RequestMapping(value = "/calculate", method=RequestMethod.POST)
+	public PaymentCalculatorOutput prepareUserExpenses(@RequestBody PaymentCalculatorInput input){
 		//TODO call calculate service;
 		
-	}
-	
-	public PaymentCalculatorOutput prepareUserPayments(List<Payment> payments){
-		PaymentCalculatorOutput output = new PaymentCalculatorOutput(payments);
+		PaymentCalculatorOutput output = new PaymentCalculatorOutput(null);
 		return output;
 	}
 }
