688f5170a07821dee926d61385707d6959784e32
Taylor Kemper <tjkemper6@gmail.com>
2016-07-16 18:29:19 -0400

    ng - calculator form functionality complete

---
+++ EasyPaymentsWeb/src/main/webapp/resources/ng/app.js
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+
+angular.module("EPApp", ["ui.router",
+                         "ui.bootstrap",
+                         "ngAnimate"
+                         ]);
+
+angular.module("EPApp")
+.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('HomeState', {
+      url: "/home",
+      controller : "HomeCtrl as hData",
+      templateUrl: "resources/ng/templates/home.html"
+    })
+    .state('CalculatorState', {
+      url: "/calculator",
+      templateUrl: "resources/ng/templates/calculator.html",
+      controller: "CalculatorCtrl as cData"
+    });
+  
+});
+
+angular.module("EPApp")
+.controller("HomeCtrl", function(){
+	
+});
+
+angular.module("EPApp")
+.controller("CalculatorCtrl", function(EPService){
+	var calculatorData = this;
+	
+	calculatorData.users = EPService.testInput.users;
+	
+	calculatorData.addUser = function(newUserName){
+		var newUser = {
+				name : newUserName,
+				myExpenses : []
+		};
+		
+		calculatorData.users.unshift(newUser);
+	}
+	
+	calculatorData.addUserExpense = function(user, newExpenseName, newExpenseAmount){
+		
+		var newExpense = {
+				name   : newExpenseName,
+				amount : newExpenseAmount
+		};
+		
+		user.myExpenses.unshift(newExpense);
+		
+	}
+	
+	calculatorData.calculate = function(){
+		
+		var input = {
+			users : calculatorData.users
+		};
+		
+		var promise = EPService.calculate(input);
+		
+		promise.then(function(response){
+			//success
+			console.log(response);
+		}, function(response){
+			//error
+		});
+		
+	}
+	
+	
+	
+	
+});
+
+
+angular.module("EPApp")
+.service("EPService", function($http){
+  var epserviceData = this;
+  
+  epserviceData.calculate = function(input){
+	  return $http({
+		  method:"POST",
+		  url:"calculate",
+		  data:input
+	  });
+  }
+  
+  epserviceData.testInput = {
+	    "users" : [
+	            
+	            {
+	                "name":"jacob",
+	                "myExpenses":[
+	                        {
+	                            "name":"cable",
+	                            "amount":150
+	                        },
+	                        {
+	                            "name":"electric",
+	                            "amount":76.50
+	                        }
+	                    ]
+	            },
+	            
+	            {
+	                "name":"Taylor",
+	                "myExpenses":[
+	                        {
+	                            "name":"trash",
+	                            "amount":45
+	                        }
+	                    ]
+	            },
+
+	            {
+	                "name":"jeff",
+	                "myExpenses":[
+	                        {
+	                            "name":"water",
+	                            "amount":230
+	                        }
+	                    ]
+	            },
+	            {
+	                "name":"david",
+	                "myExpenses":[
+	                        {
+	                            "name":"douchbag jar",
+	                            "amount":80
+	                        }
+	                    ]
+	            },
+	            {
+	                "name":"antonio",
+	                "myExpenses":[
+	                        {
+	                            "name":"nothing",
+	                            "amount":0
+	                        }
+	                    ]
+	            }
+	        
+	        ]
+	};
+});
