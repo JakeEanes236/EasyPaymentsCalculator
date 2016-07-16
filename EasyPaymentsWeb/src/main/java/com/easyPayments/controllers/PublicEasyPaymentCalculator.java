7afb0d5d61272b80e710a884ee3df7fa30115a15
Jake <jne.lacrosse@gmail.com>
2016-07-15 21:44:52 -0400

    Adding service and input output base logic (subject to change)

--- EasyPaymentsWeb/src/main/java/com/easyPayments/controllers/PublicEasyPaymentCalculator.java
+++ EasyPaymentsWeb/src/main/java/com/easyPayments/controllers/PublicEasyPaymentCalculator.java
@@ -1,12 +1,25 @@
 package com.easyPayments.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.easyPayments.TransferObjects.Payment;
+import com.easyPayments.TransferObjects.PaymentCalculatorInput;
+import com.easyPayments.TransferObjects.PaymentCalculatorOutput;
 
 @RestController
 public class PublicEasyPaymentCalculator {
 
-	//TODO 
 	
+	PaymentCalculatorInput input;
+	public void prepareUserExpenses(){
+		//TODO call calculate service;
+		
+	}
 	
-	
+	public PaymentCalculatorOutput prepareUserPayments(List<Payment> payments){
+		PaymentCalculatorOutput output = new PaymentCalculatorOutput(payments);
+		return output;
+	}
 }
---
+++ EasyPaymentsWeb/src/main/java/com/easyPayments/services/EasyCalculateService.java
@@ -0,0 +1,13 @@
+package com.easyPayments.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class EasyCalculateService {
+
+	
+	
+}
